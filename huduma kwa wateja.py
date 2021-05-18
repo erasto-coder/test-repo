e6114c743e33889c5168c0ea39269beec7b75089
erasto-code <84329463+erasto-coder@users.noreply.github.com>
2021-05-18 05:25:58 +0300

    check the code, get the code and make any changes
    This is an intermediate project which  gives the clear view of what happens back when a person needs to buy a for airtimes this is done particular for the VODACOM COMPANY
    
    so get the code make any changes and we can share ideas on that.
    precaution: know that the SIM differs from one another emplimentation will just base on a SIM type that you have Thanks.

---
+++ huduma kwa wateja.py
@@ -0,0 +1,417 @@
+
+
+
+number = str(input( "ingiza namba *149*01# kuingia kwenye menue ya vodacom :  " ))
+y = "*149*01#"
+
+if y == number:
+    
+    print("\n\n    			1. Ofa maalumu")
+    print("			    2. Ya kwako Tu")
+    print(" 			3. Nunua Bando ")
+    print(" 			4. Mnunulie Rafiki")
+    print(" 			  5. Nipige Tafu")
+    print("  			 6. Tuzo")
+    print("  			7. Huduma Extra")
+    print("			   8. Huduma Kwa Wateja")
+
+    choose = int(input("ingiza chaguo: "))    
+    if choose == 1:
+        print("\n\n        	          1. JIMIXIE")
+        print("  		       2. Nunua Vifurushi")
+        print(" 		     3. Islamic Offer ")
+        print(" 		  4. Tsh1000=100 Units/saa24")
+        print(" 		5. Tsh1000=DK60+Dk40+SMS20/saa24")
+        print("  		  6. TUSUA MAPENE(Shinda Mil.100)") 
+        print("  			   00 back")
+        print("			      99 Menu Kuu")
+
+        choose = int(input("ingiza chaguo: "))
+        if choose == 1:
+                print("\n\n  		       1. huduma ya JIMIXIE inafanyiwa maboleso mpaka tar 06/04/2021. piga *149*01# kununua vifurushi1")
+        
+        elif choose == 2:
+            print("\n\n 		  1. Tsh2000=DK30Voda+DK70(mingine)+30sms+MB400/siku7")
+            print(" 		2. Tsh500=DK60+Dk40+SMS20/saa24")
+            print("  		  3. Tsh500=130MB/saa24 ")
+            chaguo = int(input("tafadhari ingiza chaguo:"))
+            if chaguo == 1:
+                print("\n\n nunua kifurushi kupitia")
+                print("1. Muda wa maongezi")
+                print("2. MPesa")
+                chaguzi = int(input("reply..............: "))
+                if chaguzi == 1:
+                    print("\n\n ombi lako linashughurikiwa")
+                elif chaguzi == 2:
+                    pin = int(input("INGIZA NAMBA YA SIRI: "))
+                    if pin == 123:
+                        print("\n\n ombi lako linashughurikiwa...")
+                    else:
+                        print("\n\n pin uliyoingiza siyo sahihi")
+                else:
+                    print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+            elif chaguo == 2:
+                print("\n\n nunua kifurushi kupitia")
+                print("1. Muda wa maongezi")
+                print("2. MPesa")
+                chaguzi = int(input("reply..............: "))
+                if chaguzi == 1:
+                    print("\n\n ombi lako linashughurikiwa")
+                elif chaguzi == 2:
+                    pin = int(input("INGIZA NAMBA YA SIRI: "))
+                    if pin == 123:
+                        print("\n\n ombi lako linashughurikiwa...")
+                    else:
+                        print("\n\n pin uliyoingiza siyo sahihi")
+                else:
+                    print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+            elif chaguo == 3:
+                print("\n\n nunua kifurushi kupitia")
+                print("1. Muda wa maongezi")
+                print("2. MPesa")
+                chaguzi = int(input("reply..............: "))
+                if chaguzi == 1:
+                    print("\n\n ombi lako linashughurikiwa")
+                elif chaguzi == 2:
+                    pin = int(input("INGIZA NAMBA YA SIRI: "))
+                    if pin == 123:
+                        print("\n\n ombi lako linashughurikiwa...")
+                    else:
+                        print("\n\n pin uliyoingiza siyo sahihi")
+                else:
+                    print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+            else:
+                print("\n\n chaguo uliloingiza halipo!")
+
+        elif choose == 3:
+            print("\n\n              		   MAUDHUI ISLAMU BURE"  )
+            print(" 		  1. Tsh1000=dk50(mitandao yote),sms 20+10MB/saa24")	
+            print(" 		2. Tsh1000=100 Units/saa24/saa24")
+            print("  		  3. Tsh2500=dk 60(mitandao yote),SMS 100+20MB/siku7")
+            choose = int(input("ingiza chaguo: "))
+            if choose > 3:
+                print("chaguo ulilofanya siyo sahihi")
+            elif choose == 1:
+                print("\n\n nunua kifurushi kupitia")
+                print("1. Muda wa maongezi")
+                print("2. MPesa")
+                chaguzi = int(input("reply..............: "))
+                if chaguzi == 1:
+                    print("\n\n ombi lako linashughurikiwa")
+                elif chaguzi == 2:
+                    pin = int(input("INGIZA NAMBA YA SIRI: "))
+                    if pin == 123:
+                        print("\n\n ombi lako linashughurikiwa...")
+                    else:
+                        print("\n\n pin uliyoingiza siyo sahihi")
+                else:
+                    print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+                
+            elif choose == 2:
+                print("\n\n nunua kifurushi kupitia")
+                print("1. Muda wa maongezi")
+                print("2. MPesa")
+                chaguzi = int(input("reply..............: "))
+                if chaguzi == 1:
+                    print("\n\n ombi lako linashughurikiwa")
+                elif chaguzi == 2:
+                    pin = int(input("INGIZA NAMBA YA SIRI: "))
+                    if pin == 123:
+                        print("\n\n ombi lako linashughurikiwa...")
+                    else:
+                        print("\n\n pin uliyoingiza siyo sahihi")
+                else:
+                    print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+           
+            elif choose == 3:
+                print("\n\n nunua kifurushi kupitia")
+                print("1. Muda wa maongezi")
+                print("2. MPesa")
+                chaguzi = int(input("reply..............: "))
+                if chaguzi == 1:
+                    print("\n\n ombi lako linashughurikiwa")
+                elif chaguzi == 2:
+                    pin = int(input("INGIZA NAMBA YA SIRI: "))
+                    if pin == 123:
+                        print("\n\n ombi lako linashughurikiwa...")
+                    else:
+                        print("\n\n pin uliyoingiza siyo sahihi")
+                else:
+                    print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+        
+        elif choose == 4:
+            print("\n\n nunua kifurushi kupitia")
+            print("1. Muda wa maongezi")
+            print("2. MPesa")
+            chaguzi = int(input("reply..............: "))
+            if chaguzi == 1:
+                print("\n\n ombi lako linashughurikiwa")
+            elif chaguzi == 2:
+                pin = int(input("INGIZA NAMBA YA SIRI: "))
+                if pin == 123:
+                    print("\n\n ombi lako linashughurikiwa...")
+                else:
+                    print("\n\n pin uliyoingiza siyo sahihi")
+            else:
+                print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+
+        elif choose == 5:
+
+            print("\n\n nunua kifurushi kupitia")
+            print("1. Muda wa maongezi")
+            print("2. MPesa")
+            chaguzi = int(input("reply..............: "))
+            if chaguzi == 1:
+                print("\n\n ombi lako linashughurikiwa")
+            elif chaguzi == 2:
+                pin = int(input("INGIZA NAMBA YA SIRI: "))
+                if pin == 123:
+                    print("\n\n ombi lako linashughurikiwa...")
+                else:
+                    print("\n\n pin uliyoingiza siyo sahihi")
+            else:
+                print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+
+        elif choose == 6:
+            print("\n\n		  1. Tsh1800=Tiketi 7")
+            print(" 		2. Tsh6000=Tiketi 30")
+            print("  		  3. Tsh500=Tiketi 2")
+            print("  		  4. Tiketi/Dakika?MB bando")
+            print("  		  5. Tusua mapene Taarifa")
+            chaguo = int(input("please make choice: "))
+            if chaguo <= 3:
+                print("\n\n nunua kifurushi kupitia")
+                print("1. Muda wa maongezi")
+                print("2. MPesa")
+                chaguzi = int(input("reply..............: "))
+                if chaguzi == 1:
+                    print("\n\n ombi lako linashughurikiwa")
+                elif chaguzi == 2:
+                    pin = int(input("INGIZA NAMBA YA SIRI: "))
+                    if pin == 123:
+                        print("\n\n ombi lako linashughurikiwa...")
+                    else:
+                        print("\n\n pin uliyoingiza siyo sahihi")
+                else:
+                    print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+            elif chaguo == 4:
+                print("          	    1.Tusua J4U")
+                print("  		  2. Tusua Mapene Cheka")
+                print("  		  3. Tusua Mapene Pindua")
+                tus = int(input("fanya uchaguzi: "))
+                if tus == 1:
+                    print("\n\n\n 	    Tunaboresha huduma za ya kwako Tu, piga *149*01# kupata vifurushi kwa sasa.")
+                elif tus == 2:
+                    print("\n\n\n 	    1.Masaa 24")#chaguo la pili katika kipengele cha tusua mapene
+                    print("  		    2. Siku 7")
+                    print("  		    3. siku 30")
+                    muda = int(input("weka chaguo:  "))
+                    if muda == 1:
+                        print("\n\n\n 	    1. Tsh.600=Dk14(Voda-Voda)+Dk2(mitandao yote)+SMS100 Saa24+kete 1")
+                        print("  		    2. Tsh.700=Dk30(Voda-Voda)+Dk2(mitandao yote)+SMS100 Saa24+kete 1")
+                        print("  		    3.  Zaidi")
+                        print("  		    00  back")
+                        muda = int(input("weka chaguo:  "))
+                        if muda <= 2:
+                            print("\n\n nunua kifurushi kupitia")
+                            print("1. Muda wa maongezi")
+                            print("2. MPesa")
+                            chaguzi = int(input("reply..............: "))
+                            if chaguzi == 1:
+                                print("\n\n ombi lako linashughurikiwa")
+                            elif chaguzi == 2:
+                                pin = int(input("INGIZA NAMBA YA SIRI: "))
+                                if pin == 123:
+                                    print("\n\n ombi lako linashughurikiwa...")
+                                else:
+                                    print("\n\n pin uliyoingiza siyo sahihi")
+                            else:
+                                print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+
+                        if muda == 3:
+                            print(" 	    1. Tsh.1100=Dk50(Voda-Voda)+Dk5(mitandao yote)+SMS(Bilakikomo)+MB10  Saa24+kete 1")
+                            muda = int(input("ingiza chaguo: "))
+                            if muda == 1:
+                                print("\n\n nunua kifurushi kupitia")
+                                print("1. Muda wa maongezi")
+                                print("2. MPesa")
+                                chaguzi = int(input("reply..............: "))
+                                if chaguzi == 1:
+                                    print("\n\n ombi lako linashughurikiwa")
+                                elif chaguzi == 2:
+                                    pin = int(input("INGIZA NAMBA YA SIRI: "))
+                                    if pin == 123:
+                                        print("\n\n ombi lako linashughurikiwa...")
+                                    else:
+                                        print("\n\n pin uliyoingiza siyo sahihi")
+                                else:
+                                    print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+
+
+                            else:
+                                print("chaguo uliloingiza siyo sahihi....")
+                        if muda == 00:
+                            print("unajaribu kurudi nyuma.")
+
+
+                    elif muda == 2:
+                        print("      	    1. Tsh.2900=Dk50(Voda-Voda)+Dk5(mitandao yote)+SMS200 Siku 7+kete 7")
+                        print("  		    2. Tsh.4900=Dk130(Voda-Voda)+Dk5(mitandao yote)+SMS500 Siku 7+kete 7")
+                        print("  		    00  back")
+                        wiki = int(input("ingiza chaguo : "))
+                        if wiki <=3:
+                            print("\n\n nunua kifurushi kupitia")
+                            print("1. Muda wa maongezi")
+                            print("2. MPesa")
+                            chaguzi = int(input("reply..............: "))
+                            if chaguzi == 1:
+                                print("\n\n ombi lako linashughurikiwa")
+                            elif chaguzi == 2:
+                                pin = int(input("INGIZA NAMBA YA SIRI: "))
+                                if pin == 123:
+                                    print("\n\n ombi lako linashughurikiwa...")
+                                else:
+                                    print("\n\n pin uliyoingiza siyo sahihi")
+                            else:
+                                print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+                        elif wiki == 00:
+                            print("unajaruiibu kurudi nyuma...")
+                        else:
+                            print(" uchaguzi ulioufanya haupo,")
+                    elif muda == 3:
+                        print(" 1. Tsh.15000=Dk320(Voda-Voda)+Dk20(mitandao yote)+SMS200 siku30+kete 30")
+                        print(" 00 back")
+                        mwezi = int(input("ingiza namba: "))
+                        if mwezi == 1:
+                            print("\n\n nunua kifurushi kupitia")
+                            print("1. Muda wa maongezi")
+                            print("2. MPesa")
+                            chaguzi = int(input("reply..............: "))
+                            if chaguzi == 1:
+                                print("\n\n ombi lako linashughurikiwa")
+                            elif chaguzi == 2:
+                                pin = int(input("INGIZA NAMBA YA SIRI: "))
+                                if pin == 123:
+                                    print("\n\n ombi lako linashughurikiwa...")
+                                else:
+                                    print("\n\n pin uliyoingiza siyo sahihi")
+                            else:
+                                print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+
+                        elif mwezi == 00:
+                            print("unajaribu kurudi nyuma.")
+                    else:
+                        print("UMEKOSEA KUCHAGUA JARIBU TENA..")
+                elif tus == 3:
+                    print("\n\n\n 	    1. Tsh.1000=40 Units/Saa24 + Kete 1")
+                    print("  		    2.  Tsh.6000=500 Units/Siku7 + Kete 7")
+                    print("  		    3.  Tsh.20,000=1800 Units/Siku30 + Kete 30")
+                    print("  		    00  back")
+                    tus = int(input("ingiza chaguo: "))
+                    if tus <=3:
+                        print("\n\n nunua kifurushi kupitia")
+                        print("1. Muda wa maongezi")
+                        print("2. MPesa")
+                        chaguzi = int(input("reply..............: "))
+                        if chaguzi == 1:
+                            print("\n\n ombi lako linashughurikiwa")
+                        elif chaguzi == 2:
+                            pin = int(input("INGIZA NAMBA YA SIRI: "))
+                            if pin == 123:
+                                print("\n\n ombi lako linashughurikiwa...")
+                            else:
+                                print("\n\n pin uliyoingiza siyo sahihi")
+                        else:
+                            print("\n\n CHAGUO ULILOINGIZA SIYO SAHIHI..")
+
+                    elif tus == 0:
+                        print("unajaribu kurudi nyuma.")
+                    else:
+                        print("umechagua chaguo lisilo sahihi.")
+                else:
+                    print("umeingiza chaguo lisilo sahihi")
+            elif chaguo == 5:
+                print("             1. Taarifa")
+                print("  		    2. Kujitoa")
+                print("  		    00.  back")
+                tar = int(input("ingiza chaguo: "))
+                if tar == 1:
+                    print("\n\n\n 	    1. Taarifa Za Mkopo     ")
+                    chag = int(input("ingiza chaguo:"))
+                    if chag == 1:
+                        print("\n\n\n 	    Thank you for message! You will receive a confirmation SMS soon!")
+                    else:
+                        print("umeingiza chaguo kwa makosa")
+                elif tar == 2:
+                    print("\n\n\n 	    1. Kujitoa kwenye mchakato")
+                    chag = int(input("reply......:"))
+                    if chag == 1:
+                        print("\n\n\n 	    Thank you for message! You will receive a confirmation SMS soon!")
+                    else:
+                        print("chaguo halipoo...")
+                elif tar == 00:
+                    print("unajaribu kurudi nyuma.")
+                else:
+                    print("chaguo uliloingiza halipo.")              
+            else:
+                print(" CHAGUZI ULIYOINGIZA HAIPO !")
+
+                
+            
+
+        elif choose == 7:
+            print("			      unajaribu kurudi kwenye nyuma")
+
+        elif choose == 8:
+            print("			     unajaribu kurudi kwenye  Menu Kuu")
+
+        else:
+            print("\n\n CHAGUO ULILOINGIZA HALIPO")
+    elif choose == 2:
+        print("\n\n          		 Welcome:  ERASTO  MAKELEMO ")
+        print("                       		  1. Dakika ")
+        print("                        		2. Intaneti ")
+        print("                        		3. JIMIXIE ")
+        print("                           	4. MORO SUPA UNI ")
+        print("                        		5. Kijiweni ")
+        print("                    		 6. Karibu Vodacom ")
+        print("                        		 7. Chimbo ")
+        chaguo = int(input("ingiza chaguo: "))
+        if chaguo == 1:
+            print("            1. Tsh.1000=Dk40Voda")
+            print("            2. Intaneti ")
+
+       # elif chaguo == 2:
+       # elif chaguo == 3:
+        #elif chaguo == 4:
+       # elif chaguo == 5:
+        #elif chaguo == 6:
+        #elif chaguo == 7:
+        else:
+            print("eror......................")
+        
+    elif choose == 3:
+        print(" 			3. Nunua Bando ")
+    
+    elif choose == 4:
+        print(" 			4. Mnunulie Rafiki")
+    elif choose == 5:
+        print(" 			  5. Nipige Tafu")
+    elif choose == 6:
+        print("  			 6. Tuzo")
+    elif choose == 7:
+        print("  			7. Huduma Extra")
+    elif choose == 8:
+        print("			   8. Huduma Kwa Wateja")
+    else:
+        print("umeingiza chaguo lisio sahihi.")
+            
+
+
+        
+    
+if y != number:
+    print          ("|--------------------------------------------|")
+    print          ("|                invalid  number             |")
+    print          ("|           please try again thank you!      |")
+    print          ("|--------------------------------------------|") 
